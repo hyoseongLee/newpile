53c4800b3ef631da3e10ec244a624455bb06eb85
hyoseong <hyoseong123@naver.com>
2024-12-14 17:40:20 +0900

    2024.12.07

--- src/component/Todolist/Todolist.js
+++ src/component/Todolist/Todolist.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Todoitem from './Todoitem';
 import '../../CssComponent/Todolist.css';
 
-const Todolist = ({ todos }) => {
+const Todolist = ({ todos, onCheckToggle }) => {
     return (
         <div className='TodoList'>
             {todos.map(todo => (
-            <Todoitem todo={todo} key={todo.id}/>
-        ))}
+                <Todoitem
+                    todo={todo}
+                    key={todo.id}
+                    onCheckToggle={onCheckToggle} />
+            ))}
         </div>
     );
 };
--- src/page/TodolistAll.js
+++ src/page/TodolistAll.js
@@ -1,35 +1,60 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import '../CssComponent/Todolist.css';
-import Template from '../component/Todolist/Template'
-import Todolist from '../component/Todolist/Todolist'
+import Template from '../component/Todolist/Template';
+import Todolist from '../component/Todolist/Todolist';
 import { MdAddCircle } from 'react-icons/md';
+import TodoInsert from '../component/Todolist/TodoInsert';
+
+let nextId = 4;
 
 const TodolistAll = () => {
-    const [todos,setTodos] = useState(
-        [{
-            id: 1,
-            text: "할일 1",
-            checked: true
-        },
-        {
-            id: 2,
-            text: "할일 2",
-            checked: false
-        },
-        {
-            id: 3,
-            text: "할일 3",
-            checked: true
-        },
-        ]
-    )
-    return (
-        <Template todoLength={todos.length}>
-            <Todolist todos={todos} />
-            <div className='add-todo-button'>
-                <MdAddCircle /></div>
-        </Template>
-    )
+    const [insertToggle, setInsertToggle] = useState(false);
+    const [todos, setTodos] = useState([
+        { id: 1, text: "할일 1", checked: true },
+        { id: 2, text: "할일 2", checked: false },
+        { id: 3, text: "할일 3", checked: true },
+    ]);
+
+    const onInsertToggle = () => {
+        setInsertToggle(prev => !prev);
+    };
+
+    const onInsertTodo = text => {
+        if (text === "") {
+            return alert("할일을 입력해주세요");
+        } else {
+            const todo = {
+                id: nextId,
+                text,
+                checked: false
+            };
+            setTodos(Todos => Todos.concat(todo));
+            nextId++
+        }
+    };
+
+const onCheckToggle = id => {
+    setTodos(todos => todos.map(todo => todo.id === id ?
+        {...todo, checked: !todo.checked} : todo
+    ))
 }
 
-export default TodolistAll
+    return (
+        <Template todoLength={todos.length}>
+            <Todolist 
+            todos={todos}  
+            onCheckToggle={onCheckToggle}/>
+            <div className='add-todo-button' onClick={onInsertToggle}>
+                <MdAddCircle />
+            </div>
+            {insertToggle && (
+                <TodoInsert 
+                    onInsertToggle={onInsertToggle} 
+                    onInsertTodo={onInsertTodo}
+                />
+            )}
+        </Template>
+    );
+};
+
+export default TodolistAll;
